8d23db03ffe706fc8a18b6b19477ce24faefc2a7
PeterB314 <peter.bradshaw2543@gmail.com>
2025-02-10 16:40:38 +0000

    push

--- Rebuilding_Slang/submit_mnist_experiment.py
+++ Rebuilding_Slang/submit_mnist_experiment.py
@@ -12,7 +12,7 @@ def submit_python_jobs(name, variants, method='BBB', cv=0):
     subprocess.call(cmd, shell=True)
 
     basedir = os.path.dirname(os.path.abspath(__file__))
-
+    print(basedir)
     for i, variant in enumerate(variants):
         command = "python "+os.path.join(basedir, 'run_experiment.py')+" --name=\"" + str(name) + "\" --variant=\"" + str(variant) + "\" --method=\"" + str(method) + "\" --cv=" + str(cv)
         print(command)
