2c74e08d166775b278e5d8cbf62329d6e0767fb0
PeterB314 <peter.bradshaw2543@gmail.com>
2025-03-08 17:38:14 +0000

    push

---
+++ Multivariate Function/GaussianBBB.py
@@ -0,0 +1,154 @@
+import torch
+from torch import nn
+
+class MeanFieldLayer(nn.Module):
+    """Represents a mean-field Gaussian distribution over each layer of the network."""
+
+    def __init__(self, input_dim, output_dim, init_var=5e-2, prior_var = 1):
+        super(MeanFieldLayer, self).__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        
+        # Prior parameters p(theta)
+        self.w_mu_p = torch.zeros(input_dim, output_dim)
+        self.w_log_var_p = torch.ones(input_dim, output_dim) * torch.log(torch.tensor(prior_var))
+
+        # self.w_log_var_p = torch.zeros(input_dim, output_dim)
+        self.b_mu_p = torch.zeros(output_dim)
+        # self.b_log_var_p = torch.zeros(output_dim)
+        self.b_log_var_p = torch.ones(output_dim) * torch.log(torch.tensor(prior_var))
+
+
+        # Variational parameters q(theta)
+        self.w_mu_q = nn.Parameter(torch.zeros(input_dim, output_dim), requires_grad=True)
+        self.w_log_var_q = nn.Parameter(
+            torch.ones(input_dim, output_dim) * torch.log(torch.tensor(prior_var)), requires_grad=True
+        )  
+        self.b_mu_q = nn.Parameter(torch.zeros(output_dim), requires_grad=True)
+        self.b_log_var_q = nn.Parameter(
+            torch.ones(output_dim) * torch.log(torch.tensor(prior_var)), requires_grad=True
+        )
+
+    # the priors do not change so could be stored as attributes, but
+    # it feels cleaner to access them in the same way as the posteriors
+    def p_w(self):
+        """weight prior distribution"""
+        return torch.distributions.Normal(self.w_mu_p, (0.5 * self.w_log_var_p).exp())
+
+    def p_b(self):
+        """bias prior distribution"""
+        return torch.distributions.Normal(self.b_mu_p, (0.5 * self.b_log_var_p).exp())
+
+    def q_w(self):
+        """variational weight posterior"""
+        return torch.distributions.Normal(self.w_mu_q, (0.5 * self.w_log_var_q).exp())
+
+    def q_b(self):
+        """variational bias posterior"""
+        return torch.distributions.Normal(self.b_mu_q, (0.5 * self.b_log_var_q).exp())
+
+    def kl(self):
+        weight_kl = torch.distributions.kl.kl_divergence(self.q_w(), self.p_w()).sum() 
+        bias_kl = torch.distributions.kl.kl_divergence(self.q_b(), self.p_b()).sum()
+        return weight_kl + bias_kl
+
+    def forward(self, x):
+        """Propagates x through this layer by sampling weights from the posterior"""
+        assert (len(x.shape) == 3), "x should be shape (num_samples, batch_size, input_dim)."
+        assert x.shape[-1] == self.input_dim
+
+        num_samples = x.shape[0]
+        # rsample carries out reparameterisation trick for us   
+        weights = self.q_w().rsample((num_samples,))  # (num_samples, input_dim, output_dim).
+        # print('dim weights', weights.shape)
+
+        biases = self.q_b().rsample((num_samples,)).unsqueeze(1)  # (num_samples, batch_size, output_dim)
+        # print('dim biases', biases.shape)
+        # print('dim x', x.shape)
+        # print('dim x @ weights', (x @ weights).shape)
+        # print('dim x @ weights + biases', (x @ weights + biases).shape)
+        return x @ weights + biases # (num_samples, batch_size, output_dim).
+
+
+
+
+
+
+
+
+class MeanFieldBNN(nn.Module):
+    """Mean-field variational inference BNN."""
+
+    def __init__(
+        self,
+        input_dim,
+        hidden_dims,
+        output_dim,
+        activation=nn.ELU(),
+        noise_std=1.0,
+        prior_var = 1
+    ):
+        super(MeanFieldBNN, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dims = hidden_dims
+        self.output_dim = output_dim
+        self.activation = activation
+        self.log_noise_var = torch.log(torch.tensor(noise_std**2))
+        
+        self.network = nn.ModuleList()
+        for i in range(len(hidden_dims) + 1):
+            if i == 0:
+                self.network.append(MeanFieldLayer(self.input_dim, self.hidden_dims[i], prior_var = prior_var))
+                self.network.append(self.activation)
+            elif i == len(hidden_dims):
+                self.network.append(
+                    MeanFieldLayer(self.hidden_dims[i - 1], self.output_dim, prior_var = prior_var)
+                )
+            else:
+                self.network.append(
+                    MeanFieldLayer(self.hidden_dims[i - 1], self.hidden_dims[i], prior_var = prior_var)
+                )
+                self.network.append(self.activation) 
+
+    def forward(self, x, num_samples=1):
+        """Propagate the inputs through the network using num_samples weights.
+
+        Args:
+            x (torch.tensor): Inputs to the network.
+            num_samples (int, optional): Number of samples to use. Defaults to 1.
+        """
+        assert len(x.shape) == 2, "x.shape must be (batch_size, input_dim)."
+
+        # Expand dimensions of x to (num_samples, batch_size, input_dim).
+        x = torch.unsqueeze(x, 0).repeat(num_samples, 1, 1)
+        print(x.shape)
+        # Propagate x through network
+        for layer in self.network:
+            x = layer(x)
+
+        assert len(x.shape) == 3, "x.shape must be (num_samples, batch_size, output_dim)"
+        assert x.shape[-1] == self.output_dim
+
+        return x
+
+    def ll(self, y_obs, y_pred, num_samples=1):
+        """Computes the log likelihood of the outputs of self.forward(x)"""
+        l = torch.distributions.normal.Normal(y_pred, torch.sqrt(torch.exp(self.log_noise_var)))
+        
+        # take mean over num_samples dim, sum over batch_size dim
+        # note that after taking mean, batch_size becomes dim 0
+        return l.log_prob(y_obs.unsqueeze(0).repeat(num_samples, 1, 1)).mean(0).sum(0).squeeze()
+
+    def kl(self):
+        """Computes the KL divergence between the approximate posterior and the prior for the network."""
+        return sum([layer.kl() for layer in self.network if isinstance(layer, MeanFieldLayer)])
+
+    def loss(self, x, y, num_samples=1):
+        """Computes the ELBO and returns its negative"""
+
+        y_pred = self.forward(x, num_samples=num_samples)
+        
+        exp_ll = self.ll(y, y_pred, num_samples=num_samples)
+        kl = self.kl()
+
+        return kl - exp_ll, exp_ll, kl
